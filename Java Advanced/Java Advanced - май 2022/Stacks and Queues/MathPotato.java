239691fcb88547ec60e300f3ddab747e82c8a7cd
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-05-21 16:56:32 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Stacks and Queues/MathPotato.java
@@ -0,0 +1,45 @@
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Scanner;
+
+public class MathPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //save kids to queue
+        ArrayDeque<String> queue = new ArrayDeque<>(List.of(scanner.nextLine().split("\\s+")));
+
+        //Every nth toss the child left
+        int pass = Integer.parseInt(scanner.nextLine());
+
+        int cycle = 1;
+        for (int i = pass + 1; i >= 0; i++) {
+            if (queue.size() == 1) {
+                System.out.println("Last is " + queue.poll());
+                return;
+            }
+
+            if (i % pass == 0) {
+                if (!isPrime(cycle++)) {
+                    System.out.println("Removed " + queue.poll());
+                } else {
+                    System.out.println("Prime " + queue.peek());
+                }
+            } else {
+                queue.offer(queue.poll());
+            }
+        }
+    }
+    private static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
