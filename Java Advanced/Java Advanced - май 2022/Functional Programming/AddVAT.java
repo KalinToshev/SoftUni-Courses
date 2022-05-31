15a423ba15cc76fcefe25fdf27b0717cf93f962d
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-05-31 20:13:36 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Functional Programming/AddVAT.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class AddVAT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        UnaryOperator<Double> addVAT = p -> p * 1.20;
+
+        Consumer<Double> printer = d -> System.out.printf("%.2f%n", d);
+
+        System.out.println("Price with VAT:");
+
+        Arrays.stream(scanner.nextLine().split(", "))
+                .map(Double::parseDouble)
+                .map(addVAT)
+                .forEach(printer);
+    }
+}
