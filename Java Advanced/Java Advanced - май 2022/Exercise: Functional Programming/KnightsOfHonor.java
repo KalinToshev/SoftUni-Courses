46152e7f775937777b9828191372a422fa250069
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-05-31 22:11:12 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Exercise: Functional Programming/KnightsOfHonor.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = scanner.nextLine().split("\\s+");
+
+        Consumer<String> printer = name -> System.out.println("Sir " + name);
+
+        Arrays.stream(names).forEach(printer);
+    }
+}
