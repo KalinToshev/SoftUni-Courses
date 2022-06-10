53769800c265ae1219177b5398b9edb1a385d64c
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-10 19:09:17 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Generics/Jar of T/Jar.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+    private final ArrayDeque<T> elements;
+
+    public Jar() {
+        this.elements = new ArrayDeque<>();
+    }
+
+    public void add(T element) {
+        elements.push(element);
+    }
+
+    public T remove() {
+        return elements.pop();
+    }
+}
