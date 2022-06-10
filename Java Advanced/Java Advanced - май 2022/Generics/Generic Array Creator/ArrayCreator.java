d0717fb211a0927306f1b3a65638745b16f630ad
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-10 20:38:11 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Generics/Generic Array Creator/ArrayCreator.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator<T> {
+    @SuppressWarnings("unchecked")
+    public static <T> T[] create(int length, T defValue) {
+        T[] arr = (T[]) Array.newInstance(defValue.getClass(), length);
+
+        Arrays.fill(arr, defValue);
+
+        return arr;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] create(Class<T> clazz, int length, T defValue) {
+        T[] arr = (T[])Array.newInstance(clazz, length);
+
+        Arrays.fill(arr, defValue);
+
+        return arr;
+    }
+}
