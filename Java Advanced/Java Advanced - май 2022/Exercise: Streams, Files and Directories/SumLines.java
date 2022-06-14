60f5443bf1a94131fa63e6c7f39ac6e075906845
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-14 12:46:43 +0300

    Update SumLines

--- Java Advanced/Java Advanced - май 2022/Exercise: Streams, Files and Directories/SumLines.java
+++ Java Advanced/Java Advanced - май 2022/Exercise: Streams, Files and Directories/SumLines.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class SumLines {
     public static void main(String[] args) throws IOException {
         String path = "C:\\Users\\Калин\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
-        FileReader fileReader = new FileReader(path);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         String line = bufferedReader.readLine();
         while (line != null) {
             long sum = 0;
