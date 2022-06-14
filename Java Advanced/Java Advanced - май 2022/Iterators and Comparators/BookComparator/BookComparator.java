4844ab8c972787b2a2128b87879874f774eae7a4
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-14 20:51:15 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Iterators and Comparators/BookComparator/BookComparator.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book first, Book second) {
+        if (first.getTitle().compareTo(second.getTitle()) == 0) {
+            if (first.getYear() > second.getYear()) {
+                return 1;
+            } else if (first.getYear() < second.getYear()) {
+                return -1;
+            }
+            return 0;
+        } else {
+            return first.getTitle().compareTo(second.getTitle());
+        }
+    }
+}
