33a46c3e4499a25d8f4ccc16101917238a6e0dbf
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-16 02:19:55 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Exercise: Generics/Generic Swap Method String/Box.java
@@ -0,0 +1,31 @@
+package GenericSwapMethodStrings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <T> {
+    private final List<T> values = new ArrayList<>();
+
+    public Box () {
+    }
+
+    public void swap(int indexOne, int indexTwo) {
+        T one = values.get(indexOne);
+        T two = values.get(indexTwo);
+        values.set(indexOne, two);
+        values.set(indexTwo, one);
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (T value : values) {
+            stringBuilder.append(String.format("%s: %s%n", value.getClass().getName(), value));
+        }
+        return stringBuilder.toString();
+    }
+}
