ed64daa573c2263e2b6ab1f8c10b7c9ce08f476f
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-16 01:40:45 +0300

    Add files via upload

---
+++ Java Advanced/Java Advanced - май 2022/Exercise: Generics/Generic Box/Main.java
@@ -0,0 +1,19 @@
+package GenericBox;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Box box = new Box();
+
+        int n = Integer.parseInt(scanner.nextLine());
+        while (n-- > 0) {
+            String line = scanner.nextLine();
+            box.add(line);
+        }
+
+        System.out.println(box);
+    }
+}
