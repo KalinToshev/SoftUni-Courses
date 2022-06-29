927f617b4871db29178c2be4c81ede6fc9c79cd7
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-06-29 16:32:30 +0300

    Add files via upload

---
+++ Java Advanced/Java OOP - юни 2022/Encapsulation/_02_SalaryIncrease/Main.java
@@ -0,0 +1,30 @@
+package EncapsulationLab.SalaryIncrease;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+
+        List<Person> people = new ArrayList<>();
+
+        while (numberOfPeople-- > 0) {
+            String[] input = scanner.nextLine().split("\\s+");
+
+            Person personToAdd = new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]));
+            people.add(personToAdd);
+        }
+
+        double bonus = Double.parseDouble(scanner.nextLine());
+
+        people.forEach(person -> {
+            person.increaseSalary(bonus);
+            System.out.println(person);
+        });
+    }
+}
