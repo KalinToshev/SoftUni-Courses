e394b6b332e90b8de6009deba8654985fa83daf9
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-07-04 15:39:24 +0300

    Add files via upload

---
+++ Java Advanced/Java OOP - юни 2022/Inheritance/_04_RandomArrayList/Main.java
@@ -0,0 +1,8 @@
+package InheritanceLab.RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+        randomArrayList.getRandomElement();
+    }
+}
