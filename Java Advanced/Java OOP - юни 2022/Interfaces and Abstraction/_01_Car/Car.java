03d04a5c65ec2deca5976da1b31d6062e3a9084b
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-07-07 18:10:43 +0300

    Add files via upload

---
+++ Java Advanced/Java OOP - юни 2022/Interfaces and Abstraction/_01_Car/Car.java
@@ -0,0 +1,15 @@
+package InterfacesAndAbstractionLab.Car;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    int TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+}
---
+++ Java Advanced/Java OOP - юни 2022/Interfaces and Abstraction/_01_Car/Seat.java
@@ -0,0 +1,40 @@
+package InterfacesAndAbstractionLab.Car;
+
+public class Seat implements Car{
+    private String model;
+    private String color;
+    private int horsePower;
+    private String countryProduced;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires", getModel(), countryProduced(), TIRES);
+    }
+}
