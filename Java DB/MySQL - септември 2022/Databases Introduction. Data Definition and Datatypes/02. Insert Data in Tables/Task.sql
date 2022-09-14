2c50826879a23573ff4115da05e4942a3a9d89a7
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-14 14:46:37 +0300

    Update and rename test to Task.sql

---
+++ Java DB/MySQL - септември 2022/Databases Introduction. Data Definition and Datatypes/02. Insert Data in Tables/Task.sql
@@ -0,0 +1,14 @@
+INSERT INTO `employees` (`first_name`, `last_name`)
+VALUES 
+	(
+    "Kalin", 
+    "Toshev"
+    ),
+    (
+    "Miroslav", 
+    "Dimitrov"
+    ),
+    (
+    "Momchil", 
+    "Nikiforov"
+    );
