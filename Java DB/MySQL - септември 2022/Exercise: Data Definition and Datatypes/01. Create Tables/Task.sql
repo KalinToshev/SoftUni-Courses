8ae70be53d30aabad076dd47145e5973788b86a7
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-15 13:50:27 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/01. Create Tables/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/01. Create Tables/Task.sql
@@ -1,1 +1,12 @@
+CREATE TABLE `minions` 
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(55),
+    `age` INT
+);
 
+CREATE TABLE `towns` 
+(
+	`town_id` INT PRIMARY KEY,
+    `name` VARCHAR(55)
+);
