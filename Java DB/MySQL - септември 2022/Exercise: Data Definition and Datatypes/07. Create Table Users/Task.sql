7d6014eb7a1150ac8bc38e7bfcbabf916681c411
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-18 14:44:32 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/07. Create Table Users/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/07. Create Table Users/Task.sql
@@ -1,1 +1,15 @@
+CREATE TABLE `users` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(26) NOT NULL,
+    `profile_picture` BLOB,
+    `last_login_time` TIME,
+    `is_deleted` BOOLEAN
+);
 
+INSERT INTO `users` (`username`, `password`, `last_login_time`, `is_deleted`) 
+VALUES ('Medunq6996', '$p9W7SCVxwJFqW6V2!DNpkCP#7', '14:39', FALSE),
+       ('Dimitrovvv04_', 'GL&*VerS3Ak^Ks@aL5%(2Z^+m*', '14:39', TRUE),
+       ('Hamburg.gerii', '3nFW!A(k5$99t@@mpIcnhVjX3!', '14:40', TRUE),
+       ('Gosuaethelthryth', 'u&Uf84cYuQtN5I3Xxj8f7fxh4(', '14:40', TRUE),
+       ('_blitzy', 'p$4mbyP%PEct*G)v^H7pj(Q5QJ', '14:41', FALSE);
