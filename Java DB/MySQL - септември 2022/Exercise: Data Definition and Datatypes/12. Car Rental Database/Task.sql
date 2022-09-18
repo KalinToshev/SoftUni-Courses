539cb3f1748da70d76822be0bc0165fe7ba19c08
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-18 18:26:37 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/12. Car Rental Database/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/12. Car Rental Database/Task.sql
@@ -1,1 +1,84 @@
+CREATE TABLE `categories` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `category` VARCHAR(20),
+    `daily_rate` DOUBLE, 
+    `weekly_rate` DOUBLE, 
+    `monthly_rate` DOUBLE, 
+    `weekend_rate` DOUBLE   
+);
 
+INSERT INTO `categories` (`category`)
+VALUES ('TestName1'),
+       ('TestName2'),
+       ('TestName3');
+       
+CREATE TABLE `cars` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `plate_number` VARCHAR(20),
+    `make` VARCHAR(20),
+    `model` VARCHAR(20),
+    `car_year` INT,
+    `category_id` INT,
+    `doors` INT,
+    `picture` BLOB,
+    `car_condition` VARCHAR(30),
+    `available` BOOLEAN
+);
+
+INSERT INTO `cars` (`plate_number`)
+VALUES ('TestName1'),
+       ('TestName2'),
+       ('TestName3');
+       
+CREATE TABLE `employees` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(50),
+    `last_name` VARCHAR(50),
+    `title` VARCHAR(50),
+    `notes` TEXT
+);
+ 
+INSERT INTO `employees` (`first_name`, `last_name`)
+VALUES ('TestName1', 'TestName1'),
+       ('TestName2', 'TestName2'),
+       ('TestName3', 'TestName3');
+ 
+CREATE TABLE `customers` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `driver_license` VARCHAR(20),
+    `full_name` VARCHAR(50),
+    `address` VARCHAR(50),
+    `city` VARCHAR(10),
+    `zip_code` VARCHAR(10),
+    `notes` TEXT
+);
+ 
+INSERT INTO `customers` (`driver_license`, `full_name`)
+VALUES ('TestName1', 'TestName1'),
+       ('TestName2', 'TestName2'),
+       ('TestName3', 'TestName3');
+ 
+CREATE TABLE `rental_orders` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `employee_id` INT,
+    `customer_id` INT,
+    `car_id` INT,
+    `car_condition` VARCHAR(50),
+    `tank_level` VARCHAR(20),
+    `kilometrage_start` INT,
+    `kilometrage_end` INT,
+    `total_kilometrage` INT,
+    `start_date` DATE, 
+    `end_date` DATE, 
+    `total_days` INT,
+    `rate_applied` DOUBLE,
+    `tax_rate` DOUBLE,
+    `order_status` VARCHAR(20),
+    `notes` TEXT
+);
+ 
+INSERT INTO `rental_orders` (`employee_id`, `customer_id`)
+VALUES 
+(1, 2),
+(2, 3),
+(3, 1);
