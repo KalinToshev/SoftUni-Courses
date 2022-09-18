620e5883f98f02a2b6ecef552ff81d7042ae0f04
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-18 13:18:19 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/03. Insert Records in Both Tables/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Data Definition and Datatypes/03. Insert Records in Both Tables/Task.sql
@@ -1,6 +1,6 @@
 INSERT INTO `towns` (`id`, `name`)
 VALUES (1, 'Sofia'),
-	   (2, 'Plovdiv'),
+       (2, 'Plovdiv'),
        (3, 'Varna');
        
 INSERT INTO `minions` (`id`, `name`, `age`, `town_id`)
