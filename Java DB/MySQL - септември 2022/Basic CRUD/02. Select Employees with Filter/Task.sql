6f59eb8a9afbf744c65382241ecd20b3b3e12c5a
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-18 12:11:17 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Basic CRUD/02. Select Employees with Filter/Task.sql
+++ Java DB/MySQL - септември 2022/Basic CRUD/02. Select Employees with Filter/Task.sql
@@ -1,1 +1,7 @@
-
+SELECT e.`id`,
+		concat(e.`first_name`, ' ', e.`last_name`) AS `full_name`,
+        e.`job_title`,
+        e.`salary`
+FROM `employees` AS e
+WHERE e.`salary` > 1000.00
+ORDER BY e.`id`;
