5afc12922e1fdea03c793bc83410f7c0d58a60f5
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-28 19:50:54 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Data Aggregation/16. 3rd Highest Salary/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Data Aggregation/16. 3rd Highest Salary/Task.sql
@@ -1,1 +1,9 @@
-
+SELECT `department_id`, (
+	SELECT DISTINCT `salary` FROM `employees` AS e
+    WHERE e.`department_id` = `employees`.`department_id`
+    ORDER BY `salary` DESC
+    LIMIT 1 OFFSET 2
+) AS 'third_highest_salary' FROM `employees`
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
