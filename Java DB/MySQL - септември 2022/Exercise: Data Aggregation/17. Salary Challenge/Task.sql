b846af20c173901fe78c839355cbcce919ca3ae9
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-09-28 20:01:42 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Data Aggregation/17. Salary Challenge/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Data Aggregation/17. Salary Challenge/Task.sql
@@ -1,1 +1,7 @@
-
+SELECT `first_name`, `last_name`, `department_id` FROM `employees` AS `current_employee`
+WHERE `salary` > (
+SELECT AVG(`salary`) FROM `employees` AS `other_employees`
+WHERE `current_employee`.`department_id` = `other_employees`.`department_id`
+)
+ORDER BY `department_id`, `employee_id`
+LIMIT 10;
