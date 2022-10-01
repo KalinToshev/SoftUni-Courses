26e838282b9d2eacdc47248bc7278431331d49b5
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-01 18:54:17 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Table Relations/03. Many-To-Many Relationship/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Table Relations/03. Many-To-Many Relationship/Task.sql
@@ -1,1 +1,48 @@
+CREATE TABLE `students` (
+`student_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL
+);
+  
+INSERT INTO `students` (`student_id`, `name`) 
+VALUES 
+(1, 'Mila'), 
+(2, 'Toni'), 
+(3, 'Ron');
+ 
+CREATE TABLE `exams` (
+`exam_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL
+);
 
+ALTER TABLE `exams` AUTO_INCREMENT = 101;
+  
+INSERT INTO `exams` (`name`) 
+VALUES 
+('Spring MVC'), 
+('Neo4j'), 
+('Oracle 11g');
+ 
+CREATE TABLE `students_exams` (
+`student_id` INT NOT NULL,
+`exam_id` INT NOT NULL,
+   
+CONSTRAINT `pk`
+PRIMARY KEY (`student_id`, `exam_id`),
+   
+CONSTRAINT `fk_this_student`
+FOREIGN KEY (`student_id`)
+REFERENCES `students` (`student_id`), 
+   
+CONSTRAINT `fk_this_exams`
+FOREIGN KEY (`exam_id`)
+REFERENCES `exams` (`exam_id`)
+);
+   
+INSERT INTO `students_exams` (`student_id`, `exam_id`) 
+VALUES 
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103);
