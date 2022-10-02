0f9ea7b456195c5a35912f742694c8d48de2c163
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-02 18:25:33 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Subqueries and JOINs/03. Employees Without Managers/Task.sql
+++ Java DB/MySQL - септември 2022/Subqueries and JOINs/03. Employees Without Managers/Task.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+`e`.`employee_id`,
+`e`.`first_name`,
+`e`.`last_name`,
+`e`.`department_id`,
+`e`.`salary`
+FROM
+`employees` AS `e`
+RIGHT JOIN
+`departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE
+`e`.`manager_id` IS NULL;
