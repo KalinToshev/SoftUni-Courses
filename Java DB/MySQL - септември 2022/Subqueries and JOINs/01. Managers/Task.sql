2157b9e8c7bd90db2a079feb0cd369468db95afc
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-02 17:18:01 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Subqueries and JOINs/01. Managers/Task.sql
+++ Java DB/MySQL - септември 2022/Subqueries and JOINs/01. Managers/Task.sql
@@ -1,1 +1,11 @@
-
+SELECT 
+`d`.`manager_id` AS `employee_id`,
+CONCAT_WS(' ', `e`.`first_name`, `e`.`last_name`) AS `full_name`,
+`d`.`department_id` AS `department_id`,
+`d`.`name` AS `department_name`
+FROM
+`departments` AS `d`
+JOIN
+`employees` AS `e` ON `d`.`manager_id` = `e`.`employee_id`
+ORDER BY `e`.`employee_id`
+LIMIT 5;
