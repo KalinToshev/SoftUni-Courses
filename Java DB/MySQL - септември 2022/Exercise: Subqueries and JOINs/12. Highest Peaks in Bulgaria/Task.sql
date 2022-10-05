a05b29717d23a9545507a07144a36e3e67dd740e
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-05 18:38:56 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Subqueries and JOINs/12. Highest Peaks in Bulgaria/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Subqueries and JOINs/12. Highest Peaks in Bulgaria/Task.sql
@@ -1,1 +1,15 @@
-
+SELECT 
+`mc`.`country_code`,
+`m`.`mountain_range`,
+`p`.`peak_name`,
+`p`.`elevation`
+FROM
+`peaks` AS `p`
+JOIN
+`mountains` AS `m` ON `p`.`mountain_id` = `m`.`id`
+JOIN
+`mountains_countries` AS `mc` ON `m`.`id` = `mc`.`mountain_id`
+WHERE
+`mc`.`country_code` = 'BG'
+AND `p`.`elevation` > 2835
+ORDER BY `p`.`elevation` DESC;
