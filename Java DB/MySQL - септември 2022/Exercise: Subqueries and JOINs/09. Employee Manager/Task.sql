7902a14bb59830ef34f10bbd47f9184c67e2e5ea
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-05 18:20:57 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Subqueries and JOINs/09. Employee Manager/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Subqueries and JOINs/09. Employee Manager/Task.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+`e`.`employee_id`,
+`e`.`first_name`,
+`e`.`manager_id`,
+`m`.`first_name`
+FROM
+`employees` AS `e`,
+`employees` AS `m`
+WHERE
+`e`.`manager_id` = `m`.`employee_id`
+AND `e`.`manager_id` IN (3 , 7)
+ORDER BY `e`.`first_name`;
