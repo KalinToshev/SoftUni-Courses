08e2ac4415ef7ab2a68f189cdd1fa9bd2d167d21
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-06 12:30:54 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Subqueries and JOINs/16. Countries without any Mountains/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Subqueries and JOINs/16. Countries without any Mountains/Task.sql
@@ -1,1 +1,8 @@
-
+SELECT 
+COUNT(`c`.`country_name`) AS 'country_count'
+FROM
+`countries` AS `c`
+LEFT JOIN
+`mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
+WHERE
+`mc`.`mountain_id` IS NULL;
