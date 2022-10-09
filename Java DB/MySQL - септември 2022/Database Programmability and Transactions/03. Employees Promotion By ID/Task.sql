710fa6a840c17e09706167caee0f19b34687a512
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-09 14:11:20 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Database Programmability and Transactions/03. Employees Promotion By ID/Task.sql
+++ Java DB/MySQL - септември 2022/Database Programmability and Transactions/03. Employees Promotion By ID/Task.sql
@@ -1,1 +1,24 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_raise_salary_by_id`(id INT)
+BEGIN
+
+DECLARE count_by_id INT;
+    
+START TRANSACTION;
+    
+SET count_by_id := (
+SELECT COUNT(*) FROM `employees` WHERE `employee_id` = id
+);
+    
+UPDATE `employees` SET `salary` = `salary` * 1.05 WHERE `employee_id` = id;
+    
+IF (count_by_id < 1) THEN
+ROLLBACK;
+ELSE
+COMMIT;
+END IF;
+
+END $$
+
+DELIMITER ;
