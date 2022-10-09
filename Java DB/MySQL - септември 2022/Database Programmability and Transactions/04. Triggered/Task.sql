068e86e4b0b32f30824fd8c80b688cd0bf1ef02b
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-09 14:25:06 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Database Programmability and Transactions/04. Triggered/Task.sql
+++ Java DB/MySQL - септември 2022/Database Programmability and Transactions/04. Triggered/Task.sql
@@ -1,1 +1,31 @@
+CREATE TABLE `deleted_employees` (
+`employee_id` INT PRIMARY KEY AUTO_INCREMENT,
+`first_name` VARCHAR(20),
+`last_name` VARCHAR(20),
+`middle_name` VARCHAR(20),
+`job_title` VARCHAR(50),
+`department_id` INT,
+`salary` DOUBLE
+);
 
+DELIMITER $$
+
+CREATE TRIGGER `tr_deleted_employees`
+AFTER DELETE
+ON `employees`
+FOR EACH ROW
+BEGIN
+
+INSERT INTO `deleted_employees` (`first_name`, `last_name`, `middle_name`, `job_title`, `department_id`, `salary`)
+VALUES
+(OLD.`first_name`,
+OLD.`last_name`,
+OLD.`middle_name`,
+OLD.`job_title`,
+OLD.`department_id`,
+OLD.`salary`);
+
+
+END $$
+
+DELIMITER ;
