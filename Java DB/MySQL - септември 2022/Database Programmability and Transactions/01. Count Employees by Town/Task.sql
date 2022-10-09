e3d49243323b4965caebb74bf03f4352aafb0d36
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-09 11:36:16 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Database Programmability and Transactions/01. Count Employees by Town/Task.sql
+++ Java DB/MySQL - септември 2022/Database Programmability and Transactions/01. Count Employees by Town/Task.sql
@@ -1,1 +1,17 @@
+DELIMITER $$
 
+CREATE FUNCTION `ufn_count_employees_by_town`(`town_name` VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+DECLARE `emp_count` INT;
+
+SET `emp_count` := (SELECT COUNT(*) FROM `employees` AS `e`
+JOIN `addresses` AS `a` ON `a`.`address_id` = `e`.`address_id`
+JOIN `towns` AS `t` ON `t`.`town_id` = `a`.`town_id`
+WHERE `t`.`name` = `town_name`);
+    
+RETURN `emp_count`;
+END $$
+
+DELIMITER ;
