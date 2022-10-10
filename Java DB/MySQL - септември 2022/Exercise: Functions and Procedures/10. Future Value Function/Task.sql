8d8a350156026a36647c1ca05f599708b5b26df8
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 20:18:17 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/10. Future Value Function/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/10. Future Value Function/Task.sql
@@ -1,1 +1,14 @@
+DELIMITER $$
 
+CREATE FUNCTION `ufn_calculate_future_value`(sum DECIMAL(19, 4), yearly_rate DOUBLE, years INT)
+RETURNS DECIMAL(19, 4)
+DETERMINISTIC
+BEGIN 
+
+DECLARE `future_sum` DECIMAL(19, 4);
+SET `future_sum` := sum * POW(1 + yearly_rate, years);
+RETURN `future_sum`;
+
+END $$
+
+DELIMITER ;
