3097891d54606d4bba10b91dcde7585cbf5d0532
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-11 01:22:00 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/14. Money Transfer/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/14. Money Transfer/Task.sql
@@ -1,1 +1,24 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_transfer_money`(from_account_id INT, to_account_id INT, amount DOUBLE(19, 4))
+BEGIN
+	
+START TRANSACTION;
+IF
+from_account_id = to_account_id OR
+amount <= 0 OR
+(SELECT `balance` FROM `accounts` WHERE `id` = from_account_id) < amount OR
+(SELECT COUNT(`id`) FROM `accounts` WHERE `id` = from_account_id) <> 1 OR
+(SELECT COUNT(`id`) FROM `accounts` WHERE `id` = to_account_id) <> 1
+THEN ROLLBACK;
+ELSE
+UPDATE `accounts` SET `balance` = `balance` - amount
+WHERE `id` = from_account_id;
+UPDATE `accounts` SET `balance` = `balance` + amount
+WHERE `id` = to_account_id;
+COMMIT;
+END IF;
+    
+END $$
+
+DELIMITER ;
