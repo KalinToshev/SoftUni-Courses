82baed65a23590ea8a05fec042b19761ec4794d6
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 19:39:07 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/06. Employees by Salary Level/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/06. Employees by Salary Level/Task.sql
@@ -1,1 +1,12 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_get_employees_by_salary_level`(salary_level VARCHAR(10)) 
+BEGIN
+
+SELECT `first_name`, `last_name` FROM `employees`
+WHERE `ufn_get_salary_level`(`salary`) LIKE salary_level
+ORDER BY `first_name` DESC, `last_name` DESC;
+    
+END $$  
+
+DELIMITER ;
