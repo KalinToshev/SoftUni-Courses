a6a49f833293111ccb64079f403cedbc9abc41d7
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 12:40:45 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/02. Employees with Salary Above Number/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/02. Employees with Salary Above Number/Task.sql
@@ -1,1 +1,12 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_get_employees_salary_above`(target_salary DECIMAL(19, 4))
+BEGIN 
+
+SELECT `first_name`, `last_name` FROM `employees`
+WHERE `salary` >= target_salary
+ORDER BY `first_name`, `last_name`, `employee_id`;
+
+END $$
+
+DELIMITER ;
