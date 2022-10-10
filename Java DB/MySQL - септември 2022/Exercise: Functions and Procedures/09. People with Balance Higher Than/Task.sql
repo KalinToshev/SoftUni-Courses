288961cd412c9d7aca85ad8e1ff41319bfd8fe67
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 20:12:54 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/09. People with Balance Higher Than/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/09. People with Balance Higher Than/Task.sql
@@ -1,1 +1,15 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_get_holders_with_balance_higher_than`(money DECIMAL(19,4))
+BEGIN
+
+SELECT `h`.`first_name`, `h`.`last_name`
+FROM `account_holders` as `h`
+LEFT JOIN `accounts` as `a` ON `h`.`id` = `a`.`account_holder_id`
+GROUP BY `h`.`first_name`, `h`.`last_name`
+HAVING SUM(`a`.`balance`) > `money`
+ORDER BY `h`.`id`;
+    
+END $$
+
+DELIMITER ;
