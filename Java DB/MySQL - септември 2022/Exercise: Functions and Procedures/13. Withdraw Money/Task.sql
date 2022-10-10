433795ccde8e19541d7ecfc5f5521d819f4af71e
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-11 01:12:02 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/13. Withdraw Money/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/13. Withdraw Money/Task.sql
@@ -1,1 +1,16 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_withdraw_money`(account_id INT, money_amount DOUBLE(19, 4))
+BEGIN
+
+START TRANSACTION;
+IF(money_amount <= 0 OR (SELECT `balance` FROM `accounts` WHERE `id` = account_id) < money_amount) THEN
+ROLLBACK;
+ELSE
+UPDATE `accounts` AS `ac` SET `ac`.`balance` = `ac`.`balance` - money_amount
+WHERE `ac`.`id` = account_id;
+END IF;
+
+END $$
+
+DELIMITER ;
