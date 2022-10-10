33d032808ed6f6ad2b9bf7c682e916c547a13767
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 18:53:24 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/04. Employees from Town/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/04. Employees from Town/Task.sql
@@ -1,1 +1,14 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_get_employees_from_town`(town_name VARCHAR(50))
+BEGIN
+
+SELECT `e`.`first_name`, `e`.`last_name` FROM `employees` AS `e`
+JOIN `addresses` AS `a` USING (`address_id`)
+JOIN `towns` AS `t` USING (`town_id`)
+WHERE `t`.`name` = town_name
+ORDER BY `e`.`first_name`, `e`.`last_name`;
+
+END $$
+
+DELIMITER ;
