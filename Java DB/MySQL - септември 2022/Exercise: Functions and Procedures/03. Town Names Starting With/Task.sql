1198a2df14b12ff688c1c34e7e286fb0f52b3e6c
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 12:59:14 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/03. Town Names Starting With/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/03. Town Names Starting With/Task.sql
@@ -1,1 +1,12 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_get_towns_starting_with`(start_text VARCHAR(50))
+BEGIN 
+
+SELECT `name` FROM `towns`
+WHERE `name` LIKE CONCAT(start_text, '%')
+ORDER BY `name`;
+
+END $$
+
+DELIMITER ;
