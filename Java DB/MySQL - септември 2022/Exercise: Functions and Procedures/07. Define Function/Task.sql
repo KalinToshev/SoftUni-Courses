e509f21bc08c3e427aadf272db58452dd0567530
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 19:47:09 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/07. Define Function/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/07. Define Function/Task.sql
@@ -1,1 +1,12 @@
+DELIMITER $$
 
+CREATE FUNCTION `ufn_is_word_comprised`(set_of_letters VARCHAR(50), word VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+
+RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+
+END $$
+
+DELIMITER ;
