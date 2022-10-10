df477ad9d13966c68236356475508ff4be4a0908
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 20:28:55 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/11. Calculating Interest/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/11. Calculating Interest/Task.sql
@@ -1,1 +1,18 @@
+DELIMITER $$
 
+CREATE PROCEDURE `usp_calculate_future_value_for_account`(id INT, rate DECIMAL(19, 4))
+BEGIN
+
+SELECT 
+`a`.`id` AS 'account_id', 
+`ah`.`first_name`, 
+`ah`.`last_name`, 
+`a`.`balance` AS 'current_balance',
+ufn_calculate_future_value(`a`.`balance`, rate, 5) AS 'balance_in_5_years'
+FROM `account_holders` AS `ah`
+JOIN `accounts` AS `a` ON `ah`.`id` = `a`.`account_holder_id`
+WHERE `a`.`id` = id;
+
+END $$
+
+DELIMITER ;
