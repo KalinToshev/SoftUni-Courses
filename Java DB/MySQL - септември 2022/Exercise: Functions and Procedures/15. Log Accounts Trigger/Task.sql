44152b8b932bd18240e8b74de16c236ea69d3af2
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-11 01:27:57 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/15. Log Accounts Trigger/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/15. Log Accounts Trigger/Task.sql
@@ -1,1 +1,20 @@
+CREATE TABLE `logs`(
+`log_id` INT PRIMARY KEY AUTO_INCREMENT, 
+`account_id` INT NOT NULL,
+`old_sum` DECIMAL(19, 4) NOT NULL,
+`new_sum` DECIMAL(19, 4) NOT NULL
+);
 
+DELIMITER $$
+
+CREATE TRIGGER `tr_change_balance_account`
+AFTER UPDATE ON `accounts`
+FOR EACH ROW 
+BEGIN
+    
+INSERT INTO `logs`(`account_id`, `old_sum`, `new_sum`)
+VALUES (OLD.`id`, OLD.`balance`, NEW.`balance`);
+    
+END $$
+
+DELIMITER ;
