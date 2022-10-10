e51795632a9e9995f8d7921bc51e28de22b7d6ff
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-10 19:21:49 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/05. Salary Level Function/Task.sql
+++ Java DB/MySQL - септември 2022/Exercise: Functions and Procedures/05. Salary Level Function/Task.sql
@@ -1,1 +1,17 @@
+DELIMITER $$
 
+CREATE FUNCTION `ufn_get_salary_level`(salary DECIMAL(19, 4))
+RETURNS VARCHAR(10)
+DETERMINISTIC
+BEGIN
+
+DECLARE `salary_level` VARCHAR(10);
+IF salary < 30000 THEN SET `salary_level` := 'Low';
+ELSEIF salary <= 50000 THEN SET `salary_level` := 'Average';
+ELSE SET `salary_level` := 'High';
+END IF;
+RETURN `salary_level`;
+
+END $$
+
+DELIMITER ;
