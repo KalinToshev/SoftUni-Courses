7940ed5a24e64078597af660cb29901bcb7733aa
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-11 19:41:30 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 9 Feb 2020/08. The fastest player by towns/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 9 Feb 2020/08. The fastest player by towns/Task.sql
@@ -1,1 +1,9 @@
-
+SELECT MAX(`sd`.`speed`) AS 'max_speed', `tw`.`name`
+FROM `skills_data` AS `sd`
+RIGHT JOIN `players` AS `p` ON `p`.`skills_data_id` = `sd`.`id`
+RIGHT JOIN `teams` AS `t` ON `t`.`id` = `p`.`team_id`
+JOIN `stadiums` AS `s` ON `s`.`id` = `t`.`stadium_id`
+RIGHT JOIN `towns` AS `tw` ON `tw`.`id` = `s`.`town_id`
+WHERE `t`.`name` != 'Devify'
+GROUP BY `tw`.`id`
+ORDER BY `max_speed` DESC, `tw`.`name`;
