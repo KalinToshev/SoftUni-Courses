763ea7a01b88e16a1e5303d469b2f516eaa32562
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-11 19:38:39 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 9 Feb 2020/02. Insert/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 9 Feb 2020/02. Insert/Task.sql
@@ -1,1 +1,8 @@
-
+INSERT INTO `coaches` (`first_name`, `last_name`, `salary`, `coach_level`)
+(SELECT 
+`first_name`, 
+`last_name`, 
+`salary`, 
+CHAR_LENGTH(`first_name`)
+FROM `players`
+WHERE `age` >= 45);
