2111f73f8794aff79ba505b16f91765ea4a22aa3
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-11 19:42:25 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 9 Feb 2020/10. Find all players that play on stadium/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 9 Feb 2020/10. Find all players that play on stadium/Task.sql
@@ -1,1 +1,18 @@
+DELIMITER $$
 
+CREATE FUNCTION `udf_stadium_players_count`(stadium_name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+
+RETURN (
+SELECT COUNT(*) 
+FROM `players` AS `p` 
+LEFT JOIN `teams` AS `t` ON `t`.`id` = `p`.`team_id` 
+LEFT JOIN `stadiums` AS `s` ON `s`.`id` = `t`.`stadium_id` 
+WHERE `s`.`name` = stadium_name
+);
+
+END $$
+
+DELIMITER ;
