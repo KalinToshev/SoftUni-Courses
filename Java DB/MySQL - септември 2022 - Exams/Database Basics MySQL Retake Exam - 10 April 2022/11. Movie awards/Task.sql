0ecaa29fbc9f1cea37e650128e7eee346ad42569
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-12 20:20:50 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/11. Movie awards/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/11. Movie awards/Task.sql
@@ -1,1 +1,14 @@
+DELIMITER $$
 
+CREATE PROCEDURE `udp_award_movie`(movie_title VARCHAR(50))
+BEGIN
+
+UPDATE `actors` AS `a`
+JOIN `movies_actors` AS `ma` ON `a`.`id` = `ma`.`actor_id`
+JOIN `movies` AS `m` ON `ma`.`movie_id` = `m`.`id`
+SET `a`.`awards` = `a`.`awards` + 1
+WHERE `m`.`title` = movie_title;
+
+END $$
+
+DELIMITER ;
