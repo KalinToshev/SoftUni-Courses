68e83141f42dd4b3fcfdc451f31bd96fbdd1b992
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-12 19:58:54 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/09. Rating system/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/09. Rating system/Task.sql
@@ -1,1 +1,14 @@
-
+SELECT 
+`m`.`title`,
+(
+CASE
+WHEN `mai`.`rating` <= 4 THEN 'poor'
+WHEN `mai`.`rating` <= 7 THEN 'good'
+ELSE 'excellent'
+END
+) AS 'rating',
+(IF(`mai`.`has_subtitles` = 1, 'english', '-')) AS 'subtitles',
+`mai`.`budget`
+FROM `movies` AS `m`
+JOIN `movies_additional_info` AS `mai` ON `m`.`movie_info_id` = `mai`.`id`
+ORDER BY `mai`.`budget` DESC;
