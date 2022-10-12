599ec84e78819bfe567d2380418110cd79ac9a21
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-12 18:41:36 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/02. Insert/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/02. Insert/Task.sql
@@ -1,1 +1,18 @@
-
+INSERT INTO `actors` 
+(`first_name`, 
+`last_name`,
+`birthdate`,
+`height`,
+`awards`,
+`country_id`)
+(
+SELECT 
+REVERSE(`a`.`first_name`),
+REVERSE(`a`.`last_name`),
+DATE_ADD(`a`.`birthdate`, INTERVAL - 2 DAY),
+(`a`.`height` + 10),
+`a`.`country_id`,
+3
+FROM `actors` AS `a`
+WHERE `a`.`id` <= 10
+);
