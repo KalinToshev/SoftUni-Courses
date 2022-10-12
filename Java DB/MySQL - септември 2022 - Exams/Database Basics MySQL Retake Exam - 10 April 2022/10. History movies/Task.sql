4eaf1a1fd43dd411d8a72cd7deb2240374fcff1d
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-12 20:15:50 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/10. History movies/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/10. History movies/Task.sql
@@ -1,1 +1,22 @@
+CREATE FUNCTION `udf_actor_history_movies_count`(full_name VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
 
+DECLARE `history_movies` INT;
+
+SET `history_movies` := (
+SELECT count(`ma`.`movie_id`)
+FROM `movies_actors` AS `ma`
+JOIN `actors` AS `a` ON `ma`.`actor_id` = `a`.`id`
+JOIN `genres_movies` AS `gm` ON `gm`.`movie_id` = `ma`.`movie_id`
+JOIN `genres` AS `g` ON `gm`.`genre_id` = `g`.`id`
+WHERE concat_ws(' ', `a`.`first_name`, `a`.`last_name`) = full_name 
+AND 
+`g`.`name` = 'History'
+GROUP BY `ma`.`actor_id`
+);
+
+RETURN `history_movies`;
+
+END
