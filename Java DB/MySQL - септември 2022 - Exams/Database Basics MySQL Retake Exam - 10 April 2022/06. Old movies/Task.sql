60b573669f832dd17519463a33aef40a54cadf0c
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-12 19:35:23 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/06. Old movies/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/06. Old movies/Task.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+`mai`.`id`,
+`m`.`title`,
+`mai`.`runtime`,
+`mai`.`budget`,
+`mai`.`release_date`
+FROM
+`movies_additional_info` AS `mai`
+JOIN `movies` AS `m` ON `m`.`movie_info_id` = `mai`.`id`
+WHERE YEAR(`mai`.`release_date`) >= 1996 AND YEAR(`mai`.`release_date`) <= 1999
+ORDER BY `mai`.`runtime`, `mai`.`id`
+LIMIT 20;
