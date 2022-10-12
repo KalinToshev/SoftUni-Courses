75b55f81beba9be3cdf46472cd34ee3b3998a511
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-12 19:48:35 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/08. International festival/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 10 April 2022/08. International festival/Task.sql
@@ -1,1 +1,8 @@
-
+SELECT
+`c`.`name`,
+COUNT(`m`.`id`) AS 'movies_count'
+FROM `countries` AS `c`
+JOIN `movies` AS `m` ON `c`.`id` = `m`.`country_id`
+GROUP BY `c`.`id`
+HAVING `movies_count` >= 7
+ORDER BY `c`.`name` DESC;
