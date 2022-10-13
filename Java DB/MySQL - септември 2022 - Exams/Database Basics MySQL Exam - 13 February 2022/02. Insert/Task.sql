f7c1a73717b7e329148553c60106b288ddc07614
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-13 17:05:03 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 13 February 2022/02. Insert/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 13 February 2022/02. Insert/Task.sql
@@ -1,1 +1,11 @@
-
+INSERT INTO `reviews` 
+(`content`, `picture_url`, `published_at`, `rating`)
+(
+SELECT 
+SUBSTRING(`description`, 1, 15),
+REVERSE(`name`),
+'2010-10-10',
+(`price` / 8)
+FROM `products`
+WHERE `id` >= 5
+);
