8d825f7ae7df49325426278fed1469fd9630bb0b
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-13 17:07:33 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 13 February 2022/11. Reduce price/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 13 February 2022/11. Reduce price/Task.sql
@@ -1,1 +1,16 @@
+DELIMITER $$
 
+CREATE PROCEDURE `udp_reduce_price`(category_name VARCHAR(50))
+BEGIN
+
+UPDATE `products` AS `p`
+JOIN `reviews` AS `r` ON `r`.`id` = `p`.`review_id`
+JOIN `categories` AS `c` ON `c`.`id` = `p`.`category_id`
+SET `p`.`price` = `p`.`price` * 0.70
+WHERE `c`.`name` = category_name
+AND
+`r`.`rating` < 4;
+
+END $$
+
+DELIMITER ;
