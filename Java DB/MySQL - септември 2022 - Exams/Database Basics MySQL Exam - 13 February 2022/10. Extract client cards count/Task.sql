842da3c693a49502ae28e30f8b90dcc6c73e3dd9
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-13 17:07:07 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 13 February 2022/10. Extract client cards count/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 13 February 2022/10. Extract client cards count/Task.sql
@@ -1,1 +1,22 @@
+DELIMITER $$
 
+CREATE FUNCTION `udf_customer_products_count`(name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+
+DECLARE `products_count` INT;
+SET `products_count` := 
+(
+SELECT
+COUNT(`c`.`id`)
+FROM `customers` AS `c`
+JOIN `orders` AS `o` ON `c`.`id` = `o`.`customer_id`
+JOIN `orders_products` AS `op` ON `o`.`id` = `op`.`order_id`
+WHERE `c`.`first_name` = name
+);
+RETURN `products_count`;
+
+END $$
+
+DELIMITER ;
