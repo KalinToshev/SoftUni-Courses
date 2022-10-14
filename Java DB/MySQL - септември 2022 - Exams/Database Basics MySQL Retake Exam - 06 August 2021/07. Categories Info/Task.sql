5f84ae94526a3e89c18db38211726e67588ab1c0
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 15:57:05 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/07. Categories Info/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/07. Categories Info/Task.sql
@@ -1,1 +1,18 @@
-
+SELECT
+`c`.`name`,
+COUNT(`gc`.`game_id`) AS 'games_count',
+ROUND(AVG(`g`.`budget`), 2) AS 'avg_budget',
+MAX(`g`.`rating`) AS 'max_rating' 
+FROM
+`categories` AS `c`
+JOIN
+`games_categories` AS `gc` ON `c`.`id` = `gc`.`category_id`
+JOIN
+`games` AS `g` ON `g`.`id` = `gc`.`game_id`
+GROUP BY 
+`c`.`id`
+HAVING
+`max_rating` >= 9.5
+ORDER BY 
+`games_count` DESC,
+`c`.`name`;
