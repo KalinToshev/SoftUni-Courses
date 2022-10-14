10877d41f1f7b3fb5e2a34167525d1016337d370
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 21:01:46 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/08. Regular clients/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/08. Regular clients/Task.sql
@@ -1,1 +1,15 @@
-
+SELECT
+`c`.`full_name`,
+COUNT(`cour`.`car_id`) AS 'count_of_cars',
+SUM(`cour`.`bill`) AS 'total_sum'
+FROM
+`clients` AS `c`
+JOIN
+`courses` AS `cour` ON `c`.`id` = `cour`.`client_id`
+GROUP BY
+`c`.`id`
+HAVING
+SUBSTR(`c`.`full_name`, 2, 1) LIKE 'a' AND
+`count_of_cars` > 1
+ORDER BY
+`c`.`full_name`;
