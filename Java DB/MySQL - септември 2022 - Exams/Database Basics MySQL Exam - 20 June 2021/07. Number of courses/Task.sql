81377794e7c3e617a466206a09bd751f84161485
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 21:01:31 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/07. Number of courses/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/07. Number of courses/Task.sql
@@ -1,1 +1,17 @@
-
+SELECT
+`c`.`id` AS 'car_id',
+`c`.`make`,
+`c`.`mileage`,
+COUNT(`cour`.`id`) AS 'count_of_courses',
+ROUND(AVG(`cour`.`bill`), 2) AS 'avg_bill'
+FROM
+`cars` AS `c`
+LEFT JOIN
+`courses` AS `cour` ON `c`.`id` = `cour`.`car_id`
+GROUP BY 
+`c`.`id`
+HAVING
+`count_of_courses` != 2
+ORDER BY
+`count_of_courses` DESC,
+`c`.`id`;
