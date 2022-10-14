e9a97dc194974fa7dd9360e2f2e26098eec5cd38
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 21:02:20 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/10. Find all courses by client’s phone number/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/10. Find all courses by client’s phone number/Task.sql
@@ -1,1 +1,23 @@
+DELIMITER $$
 
+CREATE FUNCTION `udf_courses_by_client` (phone_num VARCHAR (20))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+
+DECLARE `number_of_courses` INT;
+SET `number_of_courses` := (
+SELECT
+COUNT(`courses`.`client_id`)
+FROM
+`clients`
+JOIN
+`courses` ON `clients`.`id` = `courses`.`client_id`
+WHERE
+`clients`.`phone_number` = phone_num
+);
+RETURN `number_of_courses`; 
+
+END $$
+
+DELIMITER ;
