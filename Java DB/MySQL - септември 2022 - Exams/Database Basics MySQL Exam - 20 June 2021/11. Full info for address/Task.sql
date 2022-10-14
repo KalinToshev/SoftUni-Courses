2a768556d400244f71915ace9a05ac60b2a55974
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 21:02:35 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/11. Full info for address/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/11. Full info for address/Task.sql
@@ -1,1 +1,37 @@
+DELIMITER $$
 
+CREATE PROCEDURE `udp_courses_by_address` (address_name VARCHAR(100))
+BEGIN
+
+SELECT
+`addresses`.`name`,
+`clients`.`full_name`,
+(
+CASE
+WHEN `courses`.`bill` <= 20 THEN 'Low'
+WHEN `courses`.`bill` <= 30 THEN 'Medium'
+ELSE 'High'
+END
+) AS 'level_of_bill',
+`cars`.`make`,
+`cars`.`condition`,
+`categories`.`name`
+FROM
+`addresses`
+JOIN
+`courses` ON `courses`.`from_address_id` = `addresses`.`id`
+JOIN
+`clients` ON `courses`.`client_id` = `clients`.`id`
+JOIN
+`cars` ON `courses`.`car_id` = `cars`.`id`
+JOIN
+`categories` ON `categories`.`id` = `cars`.`category_id`
+WHERE
+`addresses`.`name` = address_name
+ORDER BY
+`cars`.`make`,
+`clients`.`full_name`;
+
+END $$
+
+DELIMITER ;
