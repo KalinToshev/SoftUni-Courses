1d06e9e2e661e554a4d1ef1c9b318d39a958dae4
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 21:02:02 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/09. Full info for courses/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Exam - 20 June 2021/09. Full info for courses/Task.sql
@@ -1,1 +1,25 @@
-
+SELECT
+`addresses`.`name`,
+(
+CASE
+WHEN HOUR(`courses`.`start`) BETWEEN 6 AND 20 THEN 'Day'
+ELSE 'Night'
+END
+) AS 'day_time',
+`courses`.`bill`,
+`clients`.`full_name`,
+`cars`.`make`,
+`cars`.`model`,
+`categories`.`name`
+FROM
+`addresses`
+JOIN
+`courses` ON `addresses`.`id` = `courses`.`from_address_id`
+JOIN
+`clients` ON `clients`.`id` = `courses`.`client_id`
+JOIN
+`cars` ON `cars`.`id` = `courses`.`car_id`
+JOIN
+`categories` ON `categories`.`id` = `cars`.`category_id`
+ORDER BY
+`courses`.`id`;
