f704706d9ae0a9f32a343c0590ab1ab84c9c9b38
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 15:56:44 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/06. Addresses of the teams/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/06. Addresses of the teams/Task.sql
@@ -1,1 +1,12 @@
-
+SELECT
+`t`.`name` AS 'team_name',
+`a`.`name` AS 'address_name',
+CHAR_LENGTH(`a`.`name`) AS 'count_of_characters'
+FROM
+`teams` AS `t`
+JOIN
+`offices` AS `o` ON `t`.`office_id` = `o`.`id`
+JOIN
+`addresses` AS `a` ON `o`.`address_id` = `a`.`id`
+WHERE `o`.`website` IS NOT NULL
+ORDER BY `team_name`, `address_name`;
