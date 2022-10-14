d2ec9fab73dc6f1002b9e1ab1d250d010a810085
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 15:59:15 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/11. Update Budget of the Games/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/11. Update Budget of the Games/Task.sql
@@ -1,1 +1,21 @@
+DELIMITER $$
 
+CREATE PROCEDURE `udp_update_budget`(min_game_rating FLOAT)
+BEGIN
+
+UPDATE `games` AS `g`
+LEFT JOIN
+`games_categories` AS `gc` ON `g`.`id` = `gc`.`game_id`
+SET
+`g`.`budget` = `g`.`budget` + 100000,
+`g`.`release_date` = ADDDATE(`g`.`release_date`, INTERVAL 1 YEAR)
+WHERE
+`gc`.`category_id` IS NULL
+AND
+`g`.`rating` > min_game_rating
+AND
+`g`.`release_date` IS NOT NULL;
+
+END $$
+
+DELIMITER ;
