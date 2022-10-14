c890971a98399960abd8372946138e883109eed2
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 15:57:45 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/09. Full info for games/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/09. Full info for games/Task.sql
@@ -1,1 +1,26 @@
-
+SELECT
+`g`.`name`,
+(
+CASE
+WHEN `g`.`budget` < 50000 THEN 'Normal budget'
+ELSE 'Insufficient budget'
+END
+) AS 'budget_level',
+`t`.`name` AS 'team_name',
+`a`.`name`
+FROM
+`games` AS `g`
+LEFT JOIN 
+`games_categories` AS `gc` ON `g`.`id` = `gc`.`game_id`
+JOIN
+`teams` AS `t` ON `g`.`team_id` = `t`.`id`
+JOIN
+`offices` AS `o` ON `t`.`office_id` = `o`.`id`
+JOIN 
+`addresses` AS `a` ON `o`.`address_id` = `a`.`id`
+WHERE
+`g`.`release_date` IS NULL
+AND
+`gc`.`category_id` IS NULL
+ORDER BY
+`g`.`name`;
