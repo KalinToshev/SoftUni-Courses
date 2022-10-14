fb54dd0a9953b2b4d620f6717213d152456c7c5c
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 15:55:31 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/02. Insert/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/02. Insert/Task.sql
@@ -1,1 +1,10 @@
-
+INSERT INTO `games` (`name`, `rating`, `budget`, `team_id`)
+(
+SELECT
+LOWER(REVERSE(SUBSTR(`name`, 2, CHAR_LENGTH(`name`)))),
+`id`,
+(`leader_id` * 1000),
+`id`
+FROM `teams`
+WHERE `id` BETWEEN 1 AND 9
+);
