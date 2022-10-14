ae42886953f438c4548dc854796e7926783398b4
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-14 15:58:40 +0300

    Update Task.sql

--- Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/10. Find all basic information for a game/Task.sql
+++ Java DB/MySQL - септември 2022 - Exams/Database Basics MySQL Retake Exam - 06 August 2021/10. Find all basic information for a game/Task.sql
@@ -1,1 +1,27 @@
+DELIMITER $$
 
+CREATE FUNCTION `udf_game_info_by_name`(game_name VARCHAR (20))
+RETURNS TEXT
+DETERMINISTIC
+BEGIN
+
+DECLARE `result` TEXT;
+SET `result` := (
+SELECT
+CONCAT('The ', `g`.`name`, ' is developed by a ', `t`.`name`, ' in an office with an address ', `a`.`name`)
+FROM
+`games` AS `g`
+JOIN 
+`teams` AS `t` ON `g`.`team_id` = `t`.`id`
+JOIN
+`offices` AS `o` ON `t`.`office_id` = `o`.`id`
+JOIN
+`addresses` AS `a` ON `a`.`id` = `o`.`address_id`
+WHERE
+`g`.`name` = game_name
+);
+RETURN `result`;
+
+END $$
+
+DELIMITER ;
