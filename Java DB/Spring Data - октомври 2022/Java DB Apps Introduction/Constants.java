cd260829178a50cf9e70f63c34dcd0b597f59a74
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-21 20:41:30 +0300

    Add files via upload

---
+++ Java DB/Spring Data - октомври 2022/Java DB Apps Introduction/Constants.java
@@ -0,0 +1,10 @@
+enum Constants {
+    ;
+    static final  String user_key = "user";
+    static final  String user_value = "root";
+
+    static final  String user_password_key = "password";
+    static final  String user_password_value = "1234567890";
+
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+}
