d663b05cfc091ab7a106ca0e1bca0d5866c41507
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-27 19:25:45 +0300

    Add files via upload

---
+++ Java DB/Spring Data - октомври 2022/Workshop - Custom ORM/src/main/java/orm/MyConnector.java
@@ -0,0 +1,39 @@
+package orm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MyConnector {
+    private static Connection connection;
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER_KEY = "user";
+    private static final String PASSWORD_KEY = "password";
+    private static final String PARAMETERS_FOR_THE_DATABASE =
+            "?allowPublicKeyRetrieval=true&useSSL=false&createDatabaseIfNotExist=true&serverTimezone=UTC";
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+    private static final String DATABASE_NAME = "custom-orm";
+
+    private MyConnector() {
+    }
+
+    private static void createConnection() throws SQLException {
+        if (connection != null) return;
+
+        Properties properties = new Properties();
+        properties.setProperty(USER_KEY, USERNAME);
+        properties.setProperty(PASSWORD_KEY, PASSWORD);
+
+        connection = DriverManager.getConnection(JDBC_URL + DATABASE_NAME + PARAMETERS_FOR_THE_DATABASE, properties);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        createConnection();
+        return connection;
+    }
+
+}
