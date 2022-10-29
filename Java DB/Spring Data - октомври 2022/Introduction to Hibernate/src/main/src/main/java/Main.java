fa0f9e471a589b261de7e2f59434f25ab2f1a81c
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-10-29 19:37:26 +0300

    Add files via upload

---
+++ Java DB/Spring Data - октомври 2022/Introduction to Hibernate/src/main/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("soft_uni");
+
+        EntityManager em = factory.createEntityManager();
+
+    }
+}
