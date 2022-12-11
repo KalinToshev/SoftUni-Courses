d91108719263bce810410b833511aa2aaa3c758d
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2022-12-11 17:29:14 +0200

    Add files via upload

---
+++ Java DB/Workshop – MVC Project:  Next Level Technologies/src/main/java/bg/softuni/nextleveltechnologies/NextLevelTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.nextleveltechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextLevelTechnologiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextLevelTechnologiesApplication.class, args);
+    }
+
+}
---
+++ Java DB/Workshop – MVC Project:  Next Level Technologies/src/test/java/bg/softuni/nextleveltechnologies/NextLevelTechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.nextleveltechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextLevelTechnologiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
