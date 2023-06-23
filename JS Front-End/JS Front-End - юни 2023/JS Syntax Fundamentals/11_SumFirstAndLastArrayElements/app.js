8fe1df6314b2baf4345554451067f9ea339bdbb5
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2023-06-23 11:12:00 +0300

    Add files via upload

---
+++ JS Front-End/JS Front-End - юни 2023/JS Syntax Fundamentals/11_SumFirstAndLastArrayElements/app.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastArrayElements(input) {
+    let first = input[0];
+    let last = input[input.length - 1];
+    let result = first + last;
+    console.log(result);
+}
+
+sumFirstAndLastArrayElements([20, 30, 40]);
