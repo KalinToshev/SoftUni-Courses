d483dc98212fdd51e47ce6461f3d1cd795edbb50
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2023-07-13 21:17:47 +0300

    Add files via upload

---
+++ JS Front-End/JS Front-End - юни 2023/DOM and Events/09. Highlight-Active/app.js
@@ -0,0 +1,11 @@
+function focused() {
+    const inputs = Array.from(document.querySelectorAll('input'));
+    inputs.forEach((input) => {
+        input.addEventListener('focus', (e) => {
+            e.target.parentElement.className = 'focused';
+        });
+        input.addEventListener('blur', (e) => {
+            e.target.parentElement.className = '';
+        });
+    })
+}
