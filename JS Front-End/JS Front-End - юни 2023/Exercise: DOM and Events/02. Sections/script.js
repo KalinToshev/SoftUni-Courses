22acbb39465b0aa66213f051d14fca6c8019a3aa
Калин Тошев <103676095+KalinToshev@users.noreply.github.com>
2023-07-16 16:07:06 +0300

    Add files via upload

---
+++ JS Front-End/JS Front-End - юни 2023/Exercise: DOM and Events/02. Sections/script.js
@@ -0,0 +1,18 @@
+function create(words) {
+  const container = document.querySelector("#content");
+
+  words.forEach((word) => {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    p.textContent = word;
+    p.style.display = "none";
+
+    div.appendChild(p);
+    div.addEventListener("click", (e) => {
+      const invisibleParagraph = e.target.querySelector("p");
+      invisibleParagraph.style.display = "block";
+    });
+
+    container.appendChild(div);
+  });
+}
